d126fdf871d5673ccdb6fa84c233791c123723eb
unknown <huangyujun@ad.fscut.com>
2024-05-17 10:03:39 +0800

    update nuget packages

--- VSPackage/Helper/FileSystemSelectionControl.xaml.cs
+++ VSPackage/Helper/FileSystemSelectionControl.xaml.cs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using GalaSoft.MvvmLight.Command;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
